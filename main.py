93c857f53c0be4df83a81473d3b88eff6d2eb05e
Ravindu6756 <ravindusandamal49@gmail.com>
2026-09-18 01:11:16 +0530

    added save and load functions

--- main.py
+++ main.py
@@ -48,13 +48,11 @@ class LogicSandbox:
         else:
             print("\033[31mwrong component type!\033[0m")
             return
-        
         print(f"Added \033[34m{comp_type}\033[0m object named '\033[33m{name}\033[0m'")
 
     def connect(self, x_name, y_name, target_pin):
         source_obj = self.memory[x_name]
         target_obj = self.memory[y_name]
-        
         self.wires.append((source_obj, target_obj, target_pin))
         print(f"Connected wire from \033[33m{x_name}\033[0m to \033[33m{y_name}\033[0m (\033[32m{target_pin}\033[0m)")
 
@@ -62,10 +60,8 @@ class LogicSandbox:
         for _ in range(2):
             for comp in self.memory.values():
                 comp.evaluate()
-
             for source_obj, target_obj, target_pin in self.wires:
-                target_obj.inputs[target_pin] = source_obj.output_value
-                
+                target_obj.inputs[target_pin] = source_obj.output_value      
         print("\033[32mSandbox refreshed!\033[0m")
 
     def output(self, name):
@@ -76,19 +72,52 @@ class LogicSandbox:
             print(f"NODE [\033[33m{name}\033[0m]: \033[35m{comp.output_value}\033[0m")
 
     def del_wire(self):
-        print(self.wires)
+        print("didn't finish!")
 
     def save(self, name):
-        return
-
-sandbox = LogicSandbox()
+        with open('data/saves.txt', 'r', encoding='utf-8')as f:
+            for num, line in enumerate(f, start=1):
+                if (num-1) % 3 == 0:
+                    if name == line:
+                        print("Chip is already own this name!")
+                        return
+        with open('data/saves.txt', 'a') as f:
+            f.write(f"{name}\n")
+            names = self.memory.keys()
+            for i in names:  
+                f.write(f"{i},{type(self.memory[i]).__name__};")
+            f.write("\n")
+            a, b = None, None
+            for i,j,k in self.wires:
+                for name1, value in self.memory.items():
+                    if value == i:
+                        a = name1
+                    if value == j:
+                        b = name1
+                f.write(f"{a},{b},{k};")
+            f.write("\n")  
+            print("Chip saved!")
+    def load(self, memory_save, wire_save):
+        self.memory = {}
+        self.wires = []
+        for name, comp_type in memory_save:
+            if comp_type == "InputComponent":
+                comp_type = "IN"
+            elif comp_type == "OutputComponent":
+                comp_type = "OUT"
+            elif comp_type == "ProcessComponent":
+                comp_type = "NAND"
+            self.add(name,comp_type)
+        for x, y, node in wire_save:
+            self.connect(x,y,node)
+        
+        
 
 def main():
     print("-------Logic gate simulator-------" \
     "\n Version 1.0"
     "\n\ntype 'help' for instructions!"
     "\n\n",end="")
-
     while True:
         command = input("<Logic gate simulator> ")
         parts = command.strip().split(" ")
@@ -108,6 +137,34 @@ def main():
             sandbox.del_wire(parts[1], parts[2], parts[3])
         elif bc == "SAVE" and len(parts) == 2:
             sandbox.save(parts[1])
+        elif bc == "LOAD" and len(parts) == 1:     
+            print("-------Saved chips-------\n")
+            list1 = []
+            with open('data/saves.txt', 'r', encoding='utf-8')as f:
+                for num, line in enumerate(f, start=1):
+                    if (num-1) % 3 == 0:
+                        print(line)
+                        list1.append(line.strip())
+            select = input("select to load: ")
+            if select not in list1:
+                print("invalid selection!")
+                continue
+            memory_save, wire_save = None, None
+            with open('data/saves.txt', 'r', encoding='utf-8')as f:
+                for num, line in enumerate(f, start=1):
+                    if (num-1) % 3 == 0:
+                        memory_save = f.readline()
+                        wire_save = f.readline()
+            #break string into list
+            m0 = memory_save.strip().split(";")
+            m1 = wire_save.strip().split(";")
+            for i in range(len(m0)):
+                m0[i] = m0[i].split(",")
+            for i in range(len(m1)):
+                m1[i] = m1[i].split(",")
+            m0.pop()
+            m1.pop()
+            sandbox.load(m0, m1)
         elif bc == "HELP" and len(parts) == 1:
             clear_screen()
             print("\n-------Instrcutions-------\n")
@@ -117,15 +174,18 @@ def main():
                 input("\nPress any key to continue...")
                 clear_screen()
                 main()
-        elif bc == "QUIT":
+        elif bc == "QUIT" and len(parts) == 1:
             break
+        else:
+            print("Wrong command!")
 
 
     input("Press any key to continue...")
 
 def clear_screen():
     subprocess.run('cls' if os.name == 'nt' else 'clear', shell=True)
-    
+
+sandbox = LogicSandbox()    
 
 clear_screen()
 main()
